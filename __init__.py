8aeedc64caa02b5467831a1398dde49a822a7605
David <david.marx84@gmail.com>
2023-12-04 14:03:32 -0800

    install...

--- __init__.py
+++ __init__.py
@@ -1,3 +1,24 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ =["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
+# scavenged install sequence from https://github.com/FizzleDorf/ComfyUI_FizzNodes/blob/main/__init__.py
+def is_installed(package, package_overwrite=None):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        pass
+
+    package = package_overwrite or package
+
+    if spec is None:
+        print(f"Installing {package}...")
+        command = f'"{python}" -m pip install {package}'
+  
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
+
+        if result.returncode != 0:
+            print(f"Couldn't install\nCommand: {command}\nError code: {result.returncode}")
+
+# to do: read from requirements.txt
+is_installed("keyframed")
